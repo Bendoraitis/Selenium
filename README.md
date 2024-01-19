f741a1cf6dfd33b7af23b9292278ab71bc1df610
rimas <rimas@rimas.lt>
2024-01-19 22:22:18 +0200

    Config added

--- README.md
+++ README.md
@@ -3,7 +3,12 @@
 ### Using Python 3.11 version
 
 ## How to install
+get drivers from here: https://googlechromelabs.github.io/chrome-for-testing/#stable
+replace "drivers/chromedriver_v120.0.6099.109" file with new one
+
 pip install selenium
+
+pip install PyYAML
 
 ## How to run
 python main.py
--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChService
 from selenium.webdriver.firefox.service import Service as FFService
+import yaml
+config = yaml.safe_load(open("config.yml"))
 
+print(config['website']['category'])
 
 op = webdriver.ChromeOptions()
-# get drivers from here: https://googlechromelabs.github.io/chrome-for-testing/#stable
 ch_driver = webdriver.Chrome(service=ChService('drivers/chromedriver_v120.0.6099.109'), options=op)
 ch_driver.maximize_window()
-ch_driver.get("https://sportland.lt/")
+ch_driver.get(config['website']['category'])
 time.sleep(5)
 print(ch_driver.title)
-input()
+
 ch_driver.close()
 
 print(dir())
